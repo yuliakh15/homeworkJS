b574dbc686beb9138d0141da5120d2365212cf17
yuliakh15 <114945980+yuliakh15@users.noreply.github.com>
2022-10-30 17:03:23 +0200

    Add files via upload

---
+++ JS homework 1/homeworkjs1.js
@@ -0,0 +1,33 @@
+function needAction () {
+
+const fullName = 'Anonym';
+const age  = 20;
+const weight = 100;
+const height = 170;
+
+if (age < 18 && client.age > 60) {
+  console.log ('Sorry, you are not eligible for the program')
+} else {
+  console.log ('Congrats! Your registration form has been submitted')
+}
+
+
+if (weight < 60 && client.weight > 120) {
+  console.log ('Sorry, you are not eligible for the program')
+} else {
+  console.log ('Congrats! Your registration form has been submitted')
+}
+
+
+if (height < 160 && > 195) {
+  console.log ('Sorry, you are not eligible for the program')
+} else {
+  console.log ('Congrats! Your registration form has been submitted')
+}
+
+}
+
+function submit () {
+  const fullNameEl = document.getElementById ("fullName");
+  const fullName = fullNameEl.value;
+}
